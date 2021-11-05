5a7f2850ba9cffa425f50516fe60c247bf8c4cca
Jayden(Seongmin) Kim <jaydsmk@gmail.com>
2021-11-05 11:48:15 -0600

    modified

--- projects/HiddenSearchWidget/HiddenSearchWidget.js
+++
@@ -1,8 +0,0 @@
-const search = document.querySelector(".search");
-const btn = document.querySelector(".btn");
-const input = document.querySelector(".input");
-
-btn.addEventListener("click", () => {
-  search.classList.toggle("active"); // toggle: add or remove (true or false) every time the action.
-  input.focus();
-});
