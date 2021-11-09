eb1e3f17a40ae778b776a44be49e8886ec1fbec1
Jayden(Seongmin) Kim <jaydsmk@gmail.com>
2021-11-09 11:58:09 -0700

    Done #6 Project

---
+++ projects/scrollAnimation/ScrollAnimation.js
@@ -0,0 +1,20 @@
+const boxes = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    //getBoundingClientRect let the position of all of them such as top, left, right, bottom, width, height know.
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
