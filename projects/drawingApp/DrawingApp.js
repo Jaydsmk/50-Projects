0ed9240f91a642dc413755f060cb8384f35c7912
Jayden(Seongmin) Kim <jaydsmk@gmail.com>
2021-12-08 15:54:58 -0700

    Done #22 Project

--- projects/drawingApp/DrawingApp.js
+++ projects/drawingApp/DrawingApp.js
@@ -1,7 +1,13 @@
 // https://developer.mozilla.org/ko/docs/Web/API/Canvas_API
 const canvas = document.getElementById("canvas");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const sizeEl = document.getElementById("size");
+const colorEl = document.getElementById("color");
+const clearEl = document.getElementById("clear");
+
 const ctx = canvas.getContext("2d");
-console.log(canvas.width, canvas.height);
+// console.log(canvas.width, canvas.height);
 
 let size = 10;
 let isPressed = false;
@@ -28,19 +34,17 @@ canvas.addEventListener("mouseup", (e) => {
 canvas.addEventListener("mousemove", (e) => {
   const clientX = e.clientX;
   const clientY = e.clientY;
-  console.log("currX : " + clientX, "currY : " + clientY);
+  // console.log("currX : " + clientX, "currY : " + clientY);
   if (isPressed) {
     const x2 = e.offsetX;
     const y2 = e.offsetY;
     console.log(x2, y2);
 
-    if (e.offsetX < 0) {
-      drawCircle(x2, y2);
-      drawLine(x, y, x2, y2);
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
 
-      x = x2;
-      y = y2;
-    }
+    x = x2;
+    y = y2;
   }
 });
 
@@ -57,9 +61,39 @@ function drawLine(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
 
 // drawCircle(100, 200);
 // drawLine(300, 300, 300, 500);
+
+function updateSizeOnScreen() {
+  sizeEl.innerText = size;
+}
+
+increaseBtn.addEventListener("click", () => {
+  size += 5;
+  if (size > 50) {
+    size = 50;
+  }
+
+  updateSizeOnScreen();
+});
+
+decreaseBtn.addEventListener("click", () => {
+  size -= 5;
+  if (size < 5) {
+    size = 5;
+  }
+
+  updateSizeOnScreen();
+});
+
+colorEl.addEventListener("change", (e) => {
+  color = e.target.value;
+});
+
+clearEl.addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
